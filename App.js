3b8c5337b41fc386bae1eab9a3bd721c2433887f
Muhammad Mursyid <muhdmursyid8@gmail.com>
2022-05-25 23:15:00 +0800

    First changes

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  
+  const handlePress = () => console.log("Pressed Successfully");
+  
+  const alertMessage = () =>
+    Alert.alert("", "Do you want to go on a date with me?", [
+      { text: "Yes", onPress: () => console.log("Yes") },
+      { text: "No", onPress: () => console.log("No") }
+    ]);
+
+    const zyzzMessage = () =>
+    Alert.alert("", "ARE YOU FKING MIRIN BRAH?", [
+      { text: "IM FKING ZYZZ BRAH", onPress: () => console.log("Opinion accepted") },
+      { text: "WTF IS THIS SHIT KUNT", onPress: () => console.log("Opinion rejected") }
+    ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={handlePress}>Hello there</Text>
+      <TouchableOpacity onPress={handlePress}>
+        <Image source={require("./assets/zyzz.jpg")}/>
+      </TouchableOpacity>
+      <Button title="Click here to be a sick kunt" onPress={zyzzMessage} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
