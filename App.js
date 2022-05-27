3577b52f747f73153b17db017a555b8db11eabaf
Muhammad Mursyid <muhdmursyid8@gmail.com>
2022-05-28 06:48:32 +0800

    Commit before gg KL

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
+  SafeAreaView,
   Text,
   View,
   Image,
@@ -10,30 +11,44 @@ import {
 } from "react-native";
 
 export default function App() {
-  
   const handlePress = () => console.log("Pressed Successfully");
-  
-  const alertMessage = () =>
+
+  /*const alertMessage = () =>
     Alert.alert("", "Do you want to go on a date with me?", [
       { text: "Yes", onPress: () => console.log("Yes") },
       { text: "No", onPress: () => console.log("No") }
-    ]);
+    ]);*/
 
-    const zyzzMessage = () =>
+  const zyzzMessage = () =>
     Alert.alert("", "ARE YOU FKING MIRIN BRAH?", [
-      { text: "IM FKING ZYZZ BRAH", onPress: () => console.log("Opinion accepted") },
-      { text: "WTF IS THIS SHIT KUNT", onPress: () => console.log("Opinion rejected") }
+      {
+        text: "IM FKING ZYZZ BRAH",
+        onPress: () => console.log("Opinion accepted"),
+      },
+      {
+        text: "WTF IS THIS SHIT KUNT",
+        onPress: () => console.log("Opinion rejected"),
+      },
     ]);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text onPress={handlePress}>Hello there</Text>
       <TouchableOpacity onPress={handlePress}>
-        <Image source={require("./assets/zyzz.jpg")}/>
+        <Image source={require("./assets/zyzz.jpg")} />
       </TouchableOpacity>
       <Button title="Click here to be a sick kunt" onPress={zyzzMessage} />
       <StatusBar style="auto" />
-    </View>
+      <View 
+        style={{
+          backgroundColor :'dark grey',
+          width: 100,
+          height:300,
+          flex : 1,
+          flexDirection: 'row-reverse',
+          justifyContent: 'center'
+        }}/>
+    </SafeAreaView>
   );
 }
 
